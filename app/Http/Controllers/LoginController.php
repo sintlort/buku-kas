a12c37175e89472a426a6477638449eae9b7ead8
I Dewa Made Laksana Putra <61153093+sintlort@users.noreply.github.com>
2024-01-05 12:23:39 +0800

    added some function
    added logout, change password, find transaction, delete transaction, and select transaction to show.

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -32,4 +32,39 @@ class LoginController extends Controller
             ]
             ]);
     }
+    
+    public function logout(Request $request){
+        //di Validasi atau tidak dulu apa yang mau masuk
+        // selanjutnya cari user yang login saat ini dengan menggunakan Auth::user()
+        //Auth::user() akan return data user yang login saat ini
+        //Auth::id() akan return id user yang login saat ini;
+        //selanjutnya token bisa didelete dengan cara Auth::user()->tokens()->delete();
+        //Selanjutnya bisa response yang km mau
+        //jangan lupa logout itu harus post ya
+        $user = Auth::user();
+        if ($user->tokens()->delete()) {
+            return response()->json(['message' => 'success'], 200);
+        }
+        return response()->json(['message' => 'failed'], 400);
+        
+    }
+    
+    public function change_password(Request $request){
+        //Jangan lupa untuk di cek old password nya dengan current password yang digunakan pada saat login, old password yang dimaksud yaitu password yang sebelum ganti password
+        //Untuk ganti password, password harus di hash menggunakan fungsi Hash::make
+        //Setelah semua dijalankan, bisa dilakukan penyimpanan dengan menggunakan fungsi save();
+        //setelah nya dapat return response sesuai standar yang digunakan
+        $user = User::find(Auth::id());
+        if (!empty($user)) {
+            if (Hash::check($request->old_password, $user->password)) {
+                $user->password = Hash::make($request->password);
+                $user->save();
+                return response()->json(['message' => 'success', 'data' => $user], 200);
+            } else {
+                return response()->json(['message' => 'not found', 'data' => ""], 404);
+            }
+        } else {
+            return response()->json(['message' => 'failed', 'data' => ''], 400);
+        }
+    }
 }
--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -23,6 +23,7 @@ class TransactionController extends Controller
 
     /**
      * Store a newly created resource in storage.
+     * Validating files can be seen on https://laravel.com/docs/10.x/validation#validating-files
      */
     public function store(Request $request)
     {
@@ -33,6 +34,13 @@ class TransactionController extends Controller
             "receipt"        => ['nullable']
         ]);
         //belum validasi file
+        // Validasi file dapat dilakukan dengan cara enkripsi terlebih dahulu file nya di android menggunakan base32/base64
+        // selanjutnya validasi di laravel menggunakal rules required
+        // lalu di dekripsi file tersebut menggunakan base32/base64 dan di cek apakah data tersebut sebuah file atau tidak
+        // pengecekan sebuah file pada dilakukan menggunakan metode ini https://stackoverflow.com/questions/62329321/how-can-i-check-a-base64-string-is-a-filewhat-type-or-not
+        // mendapatkan 3/4 karakter pertama pada sebuah teks dapat menggunakan metode https://techvblogs.com/blog/get-first-character-from-string-php
+        // jangan lupa validasi file tersebut di android juga
+        
 
         $transaction = new Transaction();
 
@@ -66,28 +74,95 @@ class TransactionController extends Controller
 
     /**
      * Display the specified resource.
+     * 
      */
-    public function show(string $id)
+    public function show(Request $request)
     {
-        //
+        // Display spesifik data menggunakan $request
+        // jika id transaksi diletakan pada link, maka perlu string $id pada fungsi update di controller contoh : https;//yourwebsite.com/update/25
+        // jika id transaksi menjadi payload post, maka id transaksi berada pada $request
+        // lebih dinamis jika id transaksi dijadikan payload, sehingga tidak perlu mengubah link di android setiap beda id.
+        // Contoh kali ini menggunkan id transaksi yang terdapat pada payload post
+        
+        // Validasi dahulu data yang dikirimkan ke server agar tidak terjadi kasalahan dalam pencarian data, sbg contoh dikirimkan sebuah string, bukan numeric
+        // maka akan terjadi eror 500, dimana sql tidak dapat mencari id dengan menggunakan string
+        $validated = $request->validate([
+            "id_transaksi"           => ['required','numeric'],
+        ]);
+        
+        //Selanjutnya di ambil data dengan menggunakan where dan with, 'where' yaitu pencarian data dengan spesifik variable yang diinginkan
+        // 'with' yaitu mengambil data relasi yang terhubung dengan data tersebut
+        
+        $dataTransaksi = Transaction::with('nama_relasi')->where('id_transaksi',$request->id_transaksi)->get();
+        // Note : data relasi Belongs to dan Has Many itu berbeda, bisa dilakukan percobaan untuk relasi tersebut
+        // Data relasi belongs to hanya terdapat satu data
+        // Data relasi Has Many terdapat banyak data, sehingga return yang diberikan yaitu sebuah array, walaupun data yang terisi adalah 1, diperlukan looping untuk membaca data tersbut atau menggunakan indexing
+        
+        // Melakukan validasi apakah terdapat data atau tidak
+        
+        if(!empty($dataTransaksi)){
+            //jika $dataTransaksi tidak kosong/empty
+            //jalankan fungsi yang diinginkan atau return
+            return response()->json(['status'=>'200','message'=>'none','data'=>$dataTransaksi],200);
+        } else {
+            // jika $dataTransaksi kosong/empty
+            //jalankan fungsi yang diinginkan atau return
+            return response()->json(['status'=>'404','message'=>'not found','data'=>''],200);
+            //kode return/status bisa di sesuaikan dengan standar yang digunakan
+        }
     }
 
     /**
      * Update the specified resource in storage.
+     * Validation rules can be seen on https://laravel.com/docs/10.x/validation#available-validation-rules
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request)
     {
-        //
+        $validated = $request->validate([
+            "id_transaksi"           => ['required','numeric'],
+            "input_name_yang_diinginkan" => ['required','rules yang diinginkan'],
+        ]);
+        // Rules dapat dilihat pada https://laravel.com/docs/10.x/validation#available-validation-rules
+        // jika id transaksi diletakan pada link post update, maka perlu string $id pada fungsi update di controller
+        // jika id transaksi menjadi payload post, maka id transaksi berada pada $request
+        // lebih dinamis jika id transaksi dijadikan payload, sehingga tidak perlu mengubah link di android setiap beda id.
+        
+        // --Mengambil data
+        // Ambil dulu data yang mau diambil
+        $dataTransaksi = Transaction::find($request->id_transaksi);
+        // Data sudah diambil, sekarang bisa di update sesuai keiinginan
+        $dataTransaksi->nama_variable_di_table_nya = $request->input_name_yang_terdapat_pada_html_atau_blade;
+        // Melakukan save pada database dapat dilakukan dengan menggunakan fungsi save();
+        $dataTransaksi->save();
+        // Setelah update, jangan lupa return ke page atau response json untuk android
+        return response()->json(['status'=>'sukses','message'=>'none', 'data'=>$dataTransaksi],200);
+        
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Request $request)
     {
-        // Budget::destroy($id);
-
-        // return response()->json();
+        // Hapus spesifik data menggunakan $request
+        // jika id transaksi diletakan pada link, maka perlu string $id pada fungsi update di controller contoh : https;//yourwebsite.com/delete/25
+        // jika id transaksi menjadi payload post, maka id transaksi berada pada $request
+        // lebih dinamis jika id transaksi dijadikan payload, sehingga tidak perlu mengubah link di android setiap beda id.
+        // Contoh kali ini menggunkan id transaksi yang terdapat pada payload post
+        
+        // Validasi dahulu data yang dikirimkan ke server agar tidak terjadi kasalahan dalam pencarian data, sbg contoh dikirimkan sebuah string, bukan numeric
+        // maka akan terjadi eror 500, dimana sql tidak dapat mencari id dengan menggunakan string
+        $validated = $request->validate([
+            "id_transaksi"           => ['required','numeric'],
+        ]);
+        
+        // Mencari data yang diinginkan dengan menggunakan find()
+        $dataTransaksiDestroy = Transaction::find($request->id_transaksi);
+        // Hapus menggunakan delete();
+        // Terdapat beberapa cara untuk melakukan delete, antara pakai delet() atau destroy()
+        
+        $dataTransaksiDestroy->delete();
+        return response()->json(['status'=>'sukses','message'=>'none', 'data'=>''],200);
     }
 }
 
